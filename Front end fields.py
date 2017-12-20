bb57c0bd64f203d0aae0fab5db8f5c08fe3c6b48
DMR-DMR <diego.maldonado@whu.edu>
2017-12-20 23:30:35 +0100

    first list

---
+++ Front end fields.py
@@ -0,0 +1,13 @@
+# this is the input of front end fields
+#coming from the answers provided by the user in the website
+
+Hauptmieter = [Name,Street,Postcode,City,Country,DateofBirth,CityofBirth]
+Untermieter = [Name,Street,Postcode,City,Country,DateofBirth,CityofBirth]
+
+Mietobjekt = [Location,Description]
+Description = [NumRooms,NumBathrooms,Kitchen,Livingroom,Balcony,Livingroom,Terrase,other]
+Mietklauseln = [TermTime,TermStart,TermEnd,Rent,Furnished,PersonalFamily,other,KautionAmount]
+Rent = [Miete,Nebenkosten]
+Nebenkosten = [Gas,Heizung,Internet,Phone,Water]
+Rentpayment = [Iban,Accountholder,Bankname]
+Keyhandover = [Apartment,Building,Keller,Attic,Postbox]
